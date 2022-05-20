9bfc85bfceb1f95b54f2f4f62d5724a49e8ba3f9
justin.hobbs <justinhobbs@fastmail.com>
2022-05-20 10:57:24 -0500

    commit tree 67db3e7f958fe34a7ffa9b62fd31024146eeec87

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    let winningYear = record.find(record => record.result === "W");
+        if (winningYear){
+            return winningYear.year;
+        } else {
+            return undefined;
+        }
+        
+    }
+// function superbowlWin(win) {
+//     let date = win.find(pizza => pizza.result === "W" )
+// }
